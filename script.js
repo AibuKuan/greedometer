001d00e2c76801b47af8f3429d55651300db009a
Aibu Kuan <kuanaibu369@gmail.com>
2023-12-26 14:29:57 +0800

    Updated to show price chart

--- script.js
+++ script.js
@@ -83,11 +83,112 @@ $('#searchbar-form').on('submit', function(event) {
                 }
                 
                 $('#price-change').text(`${change}$${marketChange} (${change + marketChangePercent})`);
+
+                loadChart(symbol);
             });
         } else {
             console.log('Company not found');
+            alert('Company not foudn');
         }
 
         
     });
 });
+
+function loadChart(symbol) {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: `https://yahoo-finance127.p.rapidapi.com/historic/${symbol}/1d/3mo`,
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'eae20b108bmsh050c0ab157af07dp151d0cjsnf5efc3f2615f',
+            'X-RapidAPI-Host': 'yahoo-finance127.p.rapidapi.com'
+        }
+    };
+    
+    $.ajax(settings)
+        .done(function (response) {
+            console.log(response);
+            function convPrices(array) { 
+                return array.map(function(price) { 
+                    return price.toFixed(2);
+                });
+            }
+            function convTime(array) {
+                return array.map(function(timestamp) {
+                    const date = new Date(timestamp * 1000);
+                    const year = date.getFullYear();
+                    const month = String(date.getMonth() + 1).padStart(2, '0');
+                    const day = String(date.getDate()).padStart(2, '0');
+                    return `${year}-${month}-${day}`;
+                });
+            }
+            function minMaxDate(array) {
+                const minDate = new Date(Math.min(...array) * 1000);
+                const maxDate = new Date(Math.max(...array) * 1000);
+
+                const minDateString = `${minDate.getFullYear()}-${String(minDate.getMonth() + 1).padStart(2, '0')}-${String(minDate.getDate()).padStart(2, '0')}`;
+                const maxDateString = `${maxDate.getFullYear()}-${String(maxDate.getMonth() + 1).padStart(2, '0')}-${String(maxDate.getDate()).padStart(2, '0')}`;
+
+                return Array(minDateString, maxDateString);
+            }
+
+            console.log(response['meta']['symbol']);
+            console.log(response['timestamp']);
+            console.log(convPrices(response['indicators']['quote'][0]['high']));
+            console.log(minMaxDate(response['timestamp']));
+
+
+            let high = {
+                type: 'scatter',
+                mode: 'lines',
+                name: `${response['meta']['symbol']} High`,
+                x: convTime(response['timestamp']),
+                y: convPrices(response['indicators']['quote'][0]['high']),
+                line: {color: '#17BECF'}
+            }
+
+            let low = {
+                type: 'scatter',
+                mode: 'lines',
+                name: `${response['meta']['symbol']} Low`,
+                x: convTime(response['timestamp']),
+                y: convPrices(response['indicators']['quote'][0]['low']),
+                line: {color: '#7F7F7F'}
+            }
+
+            let layout = {
+                title: `${response['meta']['symbol']} Price`,
+                xaxis: {
+                    autorange: true,
+                    range: minMaxDate(response['timestamp']),
+                    rangeslider: {range: minMaxDate(response['timestamp'])},
+                    type: 'date'
+                },
+                height: 500
+            }
+
+            let config = {
+                responsive: true,
+                displaylogo: false
+            }
+
+            let data = [high, low];
+
+            Plotly.purge('chart');
+            Plotly.newPlot('chart', data, layout, config);
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            alert('Couldn\'t retrieve chart data!');
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        });
+   
+}
+
+
+
+
+
