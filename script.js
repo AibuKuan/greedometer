5cc929f5f7ce3eed01d7ba6d094b393fa6e05091
Aibu Kuan <kuanaibu369@gmail.com>
2024-01-08 10:34:11 +0800

    Some fixes on the code structure

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// get the symbol param from the url
 const urlParam = new URLSearchParams(window.location.search);
 const symbol = urlParam.get('symbol');
-let apiResponse;
 
-if (symbol) displayData(symbol);
+let apiResponse; // contains the fetched data
+
+if (symbol) displayData(symbol); // displays the data if there is a url param
 
 $('#searchbar').on('focus', function() {
     $('form div').addClass('active');
@@ -19,15 +21,14 @@ $('#searchbar').on('focus', function() {
 
     $('.recom').append(recom1);
     $('.recom').append(recom2);
-
-    
 });
 
-$('#searchbar').keypress(
-    function(event) {
-        if (event.which === 13) $(this).blur();
-    }
-);
+// unfocus the searchbar when enter key is pressed
+// $('#searchbar').keypress(
+//     function(event) {
+//         if (event.which === 13) $(this).blur();
+//     }
+// );
 
 $('#searchbar').on('blur', function() {
     $('#searchbar').attr('placeholder', 'Search');
@@ -37,17 +38,23 @@ $('#searchbar').on('blur', function() {
 
 $('#searchbar-form').on('submit', function(event) {
     event.preventDefault();
+    $('#searchbar').blur(); // unfocus the searchbar
+
+    // clears the url param
     const currentUrl = new URL(window.location.href);
     currentUrl.searchParams.delete('symbol');
     history.pushState({}, '', currentUrl.toString());
-    $('.timeseries').prop('disabled', true);
-    displayData($('#searchbar').val());
+
+    const fetchedInfo = fetchInfo($('#searchbar').val());
+    displayInfo(fetchedInfo);
+
+    apiResponse = fetchChartData($('#searchbar').val()); // stores the result into a global variable
+    generateTimeSeries();
 });
 
-function displayData(symbol) {
-    console.log('You entered ' + symbol);
-
-    const url = 'https://yahoo-finance127.p.rapidapi.com/search/' + symbol;
+// fetches the company info
+function fetchInfo(symbol) {
+    const url = 'https://yahoo-finance127.p.rapidapi.com/price/' + symbol;
 
     const settings = {
         async: true,
@@ -59,76 +66,39 @@ function displayData(symbol) {
             'X-RapidAPI-Host': 'yahoo-finance127.p.rapidapi.com'
         }
     };
-
-
+    
     $.ajax(settings).done(function (response) {
         console.log(response);
-        if (symbol.toLowerCase() === response['quotes'][0]['symbol'].toLowerCase()) {
-            console.log('Company Found!')
-            $('#comp-name').text(response['quotes'][0]['longname'] + ' (' + response['quotes'][0]['symbol'] + ')');
-
-            const url = 'https://yahoo-finance127.p.rapidapi.com/price/' + symbol;
-
-            const settings = {
-                async: true,
-                crossDomain: true,
-                url: url,
-                method: 'GET',
-                headers: {
-                    'X-RapidAPI-Key': 'eae20b108bmsh050c0ab157af07dp151d0cjsnf5efc3f2615f',
-                    'X-RapidAPI-Host': 'yahoo-finance127.p.rapidapi.com'
-                }
-            };
-            
-            $.ajax(settings).done(function (response) {
-                console.log(response);
-
-                $('#market-cap').text('$' + response['marketCap']['longFmt'] + ' (' + response['marketCap']['fmt'] + ')');
-                $('#price').text('$' + response['regularMarketPrice']['fmt']);
-
-                let change;
-                if (response['regularMarketChange']['raw'] === 0) change = '';
-                else change = (response['regularMarketChange']['raw'] > 0) ? '+' : '-';
-
-                let marketChange, marketChangePercent;
-                if (change === '-') {
-                    loadChart($('#price-change').addClass('neg-color'));
-                    marketChange = response['regularMarketChange']['fmt'].substring(1)
-                    marketChangePercent = response['regularMarketChangePercent']['fmt'].substring(1)
-                } else {
-                    loadChart($('#price-change').addClass('pos-color'));
-                    marketChange = response['regularMarketChange']['fmt'];
-                    marketChangePercent = response['regularMarketChangePercent']['fmt'];
-                }
-                
-                $('#price-change').text(`${change}$${marketChange} (${change + marketChangePercent})`);
-
-                loadChart(symbol);
-            });
-        } else {
-            console.log('Company not found');
-            alert('Company not found');
-        }
+        return response;
     });
 }
 
-function convTime(array) {
-    return array.map(function(timestamp) {
-        const date = new Date(timestamp * 1000);
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        return `${year}-${month}-${day}`;
-    });
+// displays the company info
+function displayInfo(compInfo) {
+    $('#comp-name').text(compInfo['quotes'][0]['longname'] + ' (' + compInfo['quotes'][0]['symbol'] + ')'); // not fixed
+
+    $('#market-cap').text('$' + compInfo['marketCap']['longFmt'] + ' (' + compInfo['marketCap']['fmt'] + ')');
+    $('#price').text('$' + compInfo['regularMarketPrice']['fmt']);
+
+    let change;
+    if (compInfo['regularMarketChange']['raw'] === 0) change = '';
+    else change = (compInfo['regularMarketChange']['raw'] > 0) ? '+' : '-';
+
+    let marketChange, marketChangePercent;
+    if (change === '-') {
+        loadChart($('#price-change').addClass('neg-color'));
+        marketChange = compInfo['regularMarketChange']['fmt'].substring(1)
+        marketChangePercent = compInfo['regularMarketChangePercent']['fmt'].substring(1)
+    } else {
+        loadChart($('#price-change').addClass('pos-color'));
+        marketChange = compInfo['regularMarketChange']['fmt'];
+        marketChangePercent = compInfo['regularMarketChangePercent']['fmt'];
+    }
+    
+    $('#price-change').text(`${change}$${marketChange} (${change + marketChangePercent})`);
 }
 
-function convPrices(array) { 
-    return array.map(function(price) { 
-        return price.toFixed(2);
-    });
-}
-
-function loadChart(symbol) {
+function fetchChartData(symbol) {
     const settings = {
         async: true,
         crossDomain: true,
@@ -144,118 +114,7 @@ function loadChart(symbol) {
         .done(function (response) {
             console.log(response);
             apiResponse = response;
-            
-            
-            function minMaxDate(array) {
-                const minDate = new Date(Math.min(...array) * 1000);
-                const maxDate = new Date(Math.max(...array) * 1000);
 
-                const minDateString = `${minDate.getFullYear()}-${String(minDate.getMonth() + 1).padStart(2, '0')}-${String(minDate.getDate()).padStart(2, '0')}`;
-                const maxDateString = `${maxDate.getFullYear()}-${String(maxDate.getMonth() + 1).padStart(2, '0')}-${String(maxDate.getDate()).padStart(2, '0')}`;
-
-                return Array(minDateString, maxDateString);
-            }
-
-            console.log(response['meta']['symbol']);
-            console.log(response['timestamp']);
-            console.log(convPrices(response['indicators']['quote'][0]['high']));
-            console.log(minMaxDate(response['timestamp']));
-
-
-            let high = {
-                type: 'scatter',
-                mode: 'lines',
-                name: `${response['meta']['symbol']} High`,
-                x: convTime(response['timestamp']),
-                y: convPrices(response['indicators']['quote'][0]['high']),
-                line: {color: 'rgb(51, 166, 159)'}
-            }
-
-            let low = {
-                type: 'scatter',
-                mode: 'lines',
-                name: `${response['meta']['symbol']} Low`,
-                x: convTime(response['timestamp']),
-                y: convPrices(response['indicators']['quote'][0]['low']),
-                line: {color: 'rgb(234, 89, 81)'}
-            }
-
-            let layout = {
-                title: `${response['meta']['symbol']} Price`,
-                titlefont: {
-                    color: 'hsl(0, 0%, 90%)'
-                },
-                xaxis: {
-                    autorange: true,
-                    range: minMaxDate(response['timestamp']),
-                    rangeselector: { buttons: [
-                        {
-                            count: 7,
-                            label: '1 week',
-                            step: 'day'
-                        },
-                        {
-                            count: 1,
-                            label: '1 month',
-                            step: 'month'
-                        },
-                        {
-                            count: 3,
-                            label: '3 months',
-                            step: 'month'
-                        },
-                        {
-                            count: 6,
-                            label: '6 months',
-                            step: 'month'
-                        },
-                        {
-                            count: 1,
-                            label: '1 year',
-                            step: 'year'
-                        },
-                        {
-                            count: 3,
-                            label: '3 years',
-                            step: 'year'
-                        }
-                    ]},
-                    rangeslider: {range: minMaxDate(response['timestamp'])},
-                    type: 'date',
-                    gridcolor: 'hsl(0, 0%, 50%)',
-                    minorgridcolor: 'hsl(0, 0%, 50%)',
-                    tickfont: {
-                        color: 'hsl(0, 0%, 90%)'
-                    }
-                },
-                yaxis: {
-                    gridcolor: 'hsl(0, 0%, 50%)',
-                    minorgridcolor: 'hsl(0, 0%, 50%)',
-                    tickfont: {
-                        color: 'hsl(0, 0%, 90%)'
-                    }
-                },
-                legend: {
-                    font: {
-                        color: 'hsl(0, 0%, 90%)'
-                    }
-                },
-                autosize: true,
-                plot_bgcolor: 'hsl(0, 0%, 10%)',
-                paper_bgcolor: 'hsl(0, 0%, 10%)',
-                dragmode: 'pan'
-            }
-
-            let config = {
-                responsive: true,
-                displaylogo: false,
-                modeBarButtonsToRemove: ['zoomIn2d', 'zoomOut2d', 'autoScale2d', 'resetScale2d', 'toImage']
-            }
-
-            let data = [high, low];
-
-            Plotly.purge('chart');
-            Plotly.newPlot('chart', data, layout, config);
         })
         .fail(function (jqXHR, textStatus, errorThrown) {
             alert('Couldn\'t retrieve chart data!');
@@ -266,24 +125,105 @@ function loadChart(symbol) {
    
 }
 
-function currentAndLastWeek(timestamp) {
-    timestampWeekAgo = timestamp - 604800;
-    const dateToday = new Date(timestamp * 1000);
-    const dateWeekAgo = new Date(timestampWeekAgo * 1000);
+function generateTimeSeries() {
+    let high = {
+        type: 'scatter',
+        mode: 'lines',
+        name: `${apiResponse['meta']['symbol']} High`,
+        x: convTime(apiResponse['timestamp']),
+        y: convPrices(apiResponse['indicators']['quote'][0]['high']),
+        line: {color: 'rgb(51, 166, 159)'}
+    }
 
-    const yearToday = dateToday.getFullYear();
-    const yearWeekAgo = dateWeekAgo.getFullYear();
+    let low = {
+        type: 'scatter',
+        mode: 'lines',
+        name: `${apiResponse['meta']['symbol']} Low`,
+        x: convTime(apiResponse['timestamp']),
+        y: convPrices(apiResponse['indicators']['quote'][0]['low']),
+        line: {color: 'rgb(234, 89, 81)'}
+    }
 
-    const monthToday = String(dateToday.getMonth() + 1).padStart(2, '0');
-    const monthWeekAgo = String(dateWeekAgo.getMonth() + 1).padStart(2, '0');
+    let layout = {
+        title: `${apiResponse['meta']['symbol']} Price`,
+        titlefont: {
+            color: 'hsl(0, 0%, 90%)'
+        },
+        xaxis: {
+            autorange: true,
+            range: minMaxDate(apiResponse['timestamp']),
+            rangeselector: { buttons: [
+                {
+                    count: 7,
+                    label: '1 week',
+                    step: 'day'
+                },
+                {
+                    count: 1,
+                    label: '1 month',
+                    step: 'month'
+                },
+                {
+                    count: 3,
+                    label: '3 months',
+                    step: 'month'
+                },
+                {
+                    count: 6,
+                    label: '6 months',
+                    step: 'month'
+                },
+                {
+                    count: 1,
+                    label: '1 year',
+                    step: 'year'
+                },
+                {
+                    count: 3,
+                    label: '3 years',
+                    step: 'year'
+                }
+            ]},
+            rangeslider: {range: minMaxDate(apiResponse['timestamp'])},
+            type: 'date',
+            gridcolor: 'hsl(0, 0%, 50%)',
+            minorgridcolor: 'hsl(0, 0%, 50%)',
+            tickfont: {
+                color: 'hsl(0, 0%, 90%)'
+            }
+        },
+        yaxis: {
+            gridcolor: 'hsl(0, 0%, 50%)',
+            minorgridcolor: 'hsl(0, 0%, 50%)',
+            tickfont: {
+                color: 'hsl(0, 0%, 90%)'
+            }
+        },
+        legend: {
+            font: {
+                color: 'hsl(0, 0%, 90%)'
+            }
+        },
+        autosize: true,
+        plot_bgcolor: 'hsl(0, 0%, 10%)',
+        paper_bgcolor: 'hsl(0, 0%, 10%)',
+        dragmode: 'pan'
+    }
 
-    const dayToday = String(dateToday.getDate()).padStart(2, '0');
-    const dayWeekAgo = String(dateWeekAgo.getDate()).padStart(2, '0');
-    
-    return [`${yearWeekAgo}-${monthWeekAgo}-${dayWeekAgo}`, `${yearToday}-${monthToday}-${dayToday}`];
+    let config = {
+        responsive: true,
+        displaylogo: false,
+        modeBarButtonsToRemove: ['zoomIn2d', 'zoomOut2d', 'autoScale2d', 'resetScale2d', 'toImage']
+    }
+
+    let data = [high, low];
+
+    Plotly.newPlot('chart', data, layout, config);
+    $('.timeseries').prop('disabled', true);
+    $('.candlestick').prop('disabled', false);
 }
 
-$('.candlestick').on('click', function() {
+function generateCandlestick() {
     const trace = {
         x: convTime(apiResponse['timestamp']),
 
@@ -382,6 +322,57 @@ $('.candlestick').on('click', function() {
     Plotly.newPlot('chart', data, layout, config);
     $('.timeseries').prop('disabled', false);
     $('.candlestick').prop('disabled', true);
+}
+
+function convTime(array) {
+    return array.map(function(timestamp) {
+        const date = new Date(timestamp * 1000);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    });
+}
+
+function convPrices(array) { 
+    return array.map(function(price) { 
+        return price.toFixed(2);
+    });
+}
+
+function minMaxDate(array) {
+    const minDate = new Date(Math.min(...array) * 1000);
+    const maxDate = new Date(Math.max(...array) * 1000);
+
+    const minDateString = `${minDate.getFullYear()}-${String(minDate.getMonth() + 1).padStart(2, '0')}-${String(minDate.getDate()).padStart(2, '0')}`;
+    const maxDateString = `${maxDate.getFullYear()}-${String(maxDate.getMonth() + 1).padStart(2, '0')}-${String(maxDate.getDate()).padStart(2, '0')}`;
+
+    return Array(minDateString, maxDateString);
+}
+
+function currentAndLastWeek(timestamp) {
+    timestampWeekAgo = timestamp - 604800;
+    const dateToday = new Date(timestamp * 1000);
+    const dateWeekAgo = new Date(timestampWeekAgo * 1000);
+
+    const yearToday = dateToday.getFullYear();
+    const yearWeekAgo = dateWeekAgo.getFullYear();
+
+    const monthToday = String(dateToday.getMonth() + 1).padStart(2, '0');
+    const monthWeekAgo = String(dateWeekAgo.getMonth() + 1).padStart(2, '0');
+
+    const dayToday = String(dateToday.getDate()).padStart(2, '0');
+    const dayWeekAgo = String(dateWeekAgo.getDate()).padStart(2, '0');
+    
+    return [`${yearWeekAgo}-${monthWeekAgo}-${dayWeekAgo}`, `${yearToday}-${monthToday}-${dayToday}`];
+}
+
+$('.candlestick').on('click', function() {
+    generateCandlestick();
+});
+
+$('.timeseries').on('click', function() {
+    generateTimeSeries();
 });
 
 
